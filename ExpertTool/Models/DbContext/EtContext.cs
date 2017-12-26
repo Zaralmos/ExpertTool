dc042884538ad23fcd2e09a80044377460947aff
Sergei Solovev <sergey.soloviev@inbox.ru>
2017-12-26 21:30:39 +0300

    project cleaned

--- ExpertTool/Models/DbContext/EtContext.cs
+++ ExpertTool/Models/DbContext/EtContext.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ExpertTool
+namespace ExpertTool.Models
 {
     public class EtContext : DbContext
     {
--- ExpertTool/Models/DbContext/EtContextExtensions.cs
+++ ExpertTool/Models/DbContext/EtContextExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ExpertTool
+namespace ExpertTool.Models
 {
     public static class EtContextExtensions
     {
