243d7d18dbd4e7ee518a9c72ccebaa78c6d10bf5
Sergei Solovev <sergey.soloviev@inbox.ru>
2017-12-26 21:43:24 +0300

    db set

--- ExpertTool/Startup.cs
+++ ExpertTool/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExpertTool.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,6 +23,8 @@ namespace ExpertTool
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration.GetConnectionString("DefaultString");
+            services.AddDbContext<EtContext>(options => options.UseSqlServer(connectionString));
             services.AddMvc();
         }
 
